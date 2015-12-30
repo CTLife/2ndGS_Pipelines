3e199ca2488bbc34e145d307771022adf6bc3cef
Source Codes for Computational and Theoritical Life Science <yongp@outlook.com>
2015-12-30 23:37:03 +0800

    Create RASDA-8.pl

---
+++ RASDA/RASDA-8.pl
@@ -0,0 +1,210 @@
+#!/usr/bin/env perl5
+use  strict;
+use  warnings;
+use  v5.20;
+
+
+
+
+
+###################################################################################################################################################################################################
+###################################################################################################################################################################################################
+########## Help Infromation ##########
+my $HELP_g = '
+        ------------------------------------------------------------------------------------------------------------------------------------------------------
+        ------------------------------------------------------------------------------------------------------------------------------------------------------
+        Welcome to use RASDA (RNA-Seq Data Analyzer), version 0.62, 2016-01-13.      
+        RASDA is a Pipeline for Single-end and Paired-end RNA-Seq Data Analysis by Integrating Lots of Softwares.
+
+        Step 8: Convert BAM to BED format.
+        Usage:  
+               perl  RASDA-8.pl    [-v]    [-h]    [-i inputDir]    [-o outDir]   
+        For instance: 
+                     perl  RASDA-8.pl    -i 7-FinalBAM/2-Subjunc          -o 9-BED                      
+                     perl  RASDA-8.pl    --input 7-FinalBAM/2-Subjunc     --output 9-BED     
+                     perl  RASDA-8.pl    --input 7-FinalBAM/2-Subjunc     --output 9-BED    >> RASDA-8.runLog  2>&1
+     
+        -------------------------------------------------------------------------------------------------------------------
+        Optional arguments:
+        -v, --version        Show version number of this program and exit.
+
+        -h, --help           Show this help message and exit.
+
+        Required arguments:
+        -i inputDir,  --input inputDir        inputDir is the name of your input folder that contains your BAM files,
+                                              the suffix of the BAM files must be ".bam".    (no default)
+
+        -o outDir,  --output outDir           outDir is the name of your output folder that contains running 
+                                              results (BED format) of this step.      (no default) 
+        ------------------------------------------------------------------------------------------------------------------
+
+        For more details about this pipeline and other NGS data analysis piplines such as CISDA, MESDA and HISDA,
+        please see the web site: https://github.com/CTLife/2ndGS_Pipelines
+
+        Yong Peng @ He lab, yongp@outlook.com, Academy for Advanced Interdisciplinary Studies 
+        and Peking-Tsinghua Center for Life Sciences (CLS), Peking University, China.    
+        ------------------------------------------------------------------------------------------------------------------------------------------------------
+        ------------------------------------------------------------------------------------------------------------------------------------------------------  
+';
+
+
+########## Version Infromation ##########
+my $version_g = "  The Eighth Step of RASDA (RNA-Seq Data Analyzer), version 0.62, 2016-01-13.";
+
+
+########## Keys and Values ##########
+if ($#ARGV   == -1) { print  "\n$HELP_g\n\n";  exit 0; }       ## when there are no any command argumants.
+if ($#ARGV%2 ==  0) { @ARGV = (@ARGV, "-h");           }       ## when the number of command argumants is odd. 
+my %args = @ARGV;
+
+
+########## Initialize  Variables ##########
+my $input_g   = '7-FinalBAM/2-Subjunc';      ## This is only an initialization  value or suggesting value, not default value.
+my $output_g  = '9-BED';                  ## This is only an initialization  value or suggesting value, not default value.
+
+
+########## Available Arguments ##########
+my $available = "  -v  --version    -h  --help    -i  --input    -o    --output     -L  --fragLen     ";
+my $boole_g = 0;
+while( my ($key, $value) = each %args ) {
+    if($available !~ m/\s$key\s/) {print  "    Cann't recognize $key !!\n";  $boole_g = 1; }
+}
+if($boole_g == 1) {
+    print "\n    The Command Line Arguments are wrong!\n";
+    print   '    Please see help message by using "perl  RASDA-8.pl  -h". ';
+    print "\n\n";
+    exit 0;
+}
+
+
+########## Get Arguments ##########
+if ( ( exists $args{'-v' } )  or  ( exists $args{'--version'      } )  )     { print  "\n$version_g\n\n";    exit 0; }
+if ( ( exists $args{'-h' } )  or  ( exists $args{'--help'         } )  )     { print  "\n$HELP_g\n\n";       exit 0; }
+if ( ( exists $args{'-i' } )  or  ( exists $args{'--input'        } )  )     { ($input_g  = $args{'-i' })   or  ($input_g  = $args{'--input'  });  }else{print   "\n -i or --input   is required.\n\n";   print  "\n$HELP_g\n\n";       exit 0; }                                               
+if ( ( exists $args{'-o' } )  or  ( exists $args{'--output'       } )  )     { ($output_g = $args{'-o' })   or  ($output_g = $args{'--output' });  }else{print   "\n -o or --output  is required.\n\n";   print  "\n$HELP_g\n\n";       exit 0; }      
+
+
+########### Conditions #############
+$input_g   =~ m/^\S+$/   ||  die   "\n$HELP_g\n\n";
+$output_g  =~ m/^\S+$/   ||  die   "\n$HELP_g\n\n";
+
+
+######### Print Command Arguments to Standard Output ###########
+print  "\n\n
+        ################ Your Arguments ###############################
+                Input  folder:  $input_g
+                Output folder:  $output_g
+        ###############################################################  
+\n\n";
+
+
+###################################################################################################################################################################################################
+###################################################################################################################################################################################################
+
+
+
+
+
+print "\n\n\n\n\n##################################################################################################\n";
+print   "\nRunning......\n";
+my $output2_g = "$output_g/Results";
+if ( !(-e $output_g) )   { mkdir $output_g    ||  die; }
+if ( !(-e $output2_g))   { mkdir $output2_g   ||  die; }
+(-e $output_g)   ||  die;
+my $pattern = "[-.0-9A-Za-z]+";
+
+
+
+
+
+
+
+print "\n\n\n\n\n##################################################################################################\n";
+print("\nChecking all the necessary softwares in this step......\n");
+
+system("bedtools   bamtobed  -h    >> $output2_g/z-version_softwares.txt   2>&1");
+system("echo    '\n\n\n\n\n\n'     >> $output2_g/z-version_softwares.txt   2>&1");
+system("echo    '##############################################################################'  >> $output2_g/z-version_softwares.txt   2>&1");
+system("qualimap   rnaseq  -h      >> $output2_g/z-version_softwares.txt   2>&1");
+system("echo    '\n\n\n\n\n\n'     >> $output2_g/z-version_softwares.txt   2>&1");
+system("echo    '##############################################################################'  >> $output2_g/z-version_softwares.txt   2>&1");
+
+
+
+
+
+
+
+opendir(my $DH_input, $input_g) || die;     
+my @inputFiles = readdir($DH_input);
+
+print("\nChecking all the input file names......\n");
+my $fileNameBool = 1;
+for ( my $i=0; $i<=$#inputFiles; $i++ ) {  
+        next unless $inputFiles[$i] =~ m/^(\d{2})_/;   
+        next unless $inputFiles[$i] =~ m/\.bam$/;   
+        next unless $inputFiles[$i] !~ m/^[.]/;
+        next unless $inputFiles[$i] !~ m/[~]$/;
+        next unless $inputFiles[$i] !~ m/^unpaired/;
+        my $temp = $inputFiles[$i]; 
+        $temp =~ m/^(\d{2})_($pattern)_($pattern)_($pattern)_($pattern)_($pattern)_(Rep[1-9]).bam$/   or  die  "wrong-1: ## $temp ##";
+        if($temp =~ m/^(\d{2})_($pattern)_($pattern)_($pattern)_($pattern)_($pattern)_(Rep[1-9])\.bam$/) {
+             print("$inputFiles[$i]......\n");
+        }else{
+             $fileNameBool = 0;
+        }
+}
+if($fileNameBool == 1)  {print("    All the file names are passed.\n\n");}
+
+
+
+print "\n\nConvert BAM to BigWig ......\n";
+for(my $i=0; $i<=$#inputFiles; $i++) {
+    next unless $inputFiles[$i] =~ m/\.bam$/;
+    next unless $inputFiles[$i] !~ m/^[.]/;
+    next unless $inputFiles[$i] !~ m/[~]$/;
+    my $temp = $inputFiles[$i];
+    $temp =~ s/\.bam$//  or  die;
+
+    system("bedtools   bamtobed   -i $input_g/$temp.bam     > $output_g/$temp.bed");
+    system("qualimap   rnaseq  --algorithm uniquely-mapped-reads    -bam $input_g/$temp.bam    -gtf 0-Other/mm9-RefSeq.gtf   -oc computedCounts.txt  -outdir $output2_g/$temp    --sequencing-protocol non-strand-specific    --java-mem-size=5G"); 
+    system("qualimap   rnaseq  --algorithm uniquely-mapped-reads    -bam $input_g/$temp.bam    -gtf 0-Other/mm9-RefSeq.gtf   -oc computedCounts.txt  -outdir $output2_g/$temp-2  --sequencing-protocol non-strand-specific    --java-mem-size=5G  --paired"); 
+}
+
+
+
+
+
+
+
+print "\n\n\n\n\n##################################################################################################\n";
+print "\n\n        Job Done! Cheers! \n\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
