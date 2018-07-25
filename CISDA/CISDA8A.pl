737eeda607863992b28c6df902be5485db94e67a
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2018-07-25 11:12:51 +0800

    v0.9.4

--- CISDA/CISDA8A.pl
+++ CISDA/CISDA8A.pl
@@ -2,6 +2,7 @@
 use  strict;
 use  warnings;
 use  v5.22;
+use  String::Similarity;
 
 ## Perl5 version >= 5.22
 ## You can create a symbolic link for perl5 by using "sudo  ln  /usr/bin/perl   /usr/bin/perl5" in Ubuntu.
@@ -14,23 +15,23 @@ use  v5.22;
 
 ###################################################################################################################################################################################################
 my $genome_g = '';  ## such as "mm10", "ce11", "hg38".
-my $input_g  = '';  ## such as "7-finalBAM/1_Trim_BWAmem/callPeaks"
-my $output_g = '';  ## such as "12A-MACS2/1_Trim_BWAmem"
+my $input_g  = '';  ## such as "6-BAMPE/1_BWAmem"
+my $output_g = '';  ## such as "9A-MACS2/1_BWAmem"
 
 {
 ## Help Infromation
 my $HELP = '
         ------------------------------------------------------------------------------------------------------------------------------------------------------
         ------------------------------------------------------------------------------------------------------------------------------------------------------
-        Welcome to use CISDA (ChIP-Seq Data Analyzer), version 0.9.0, 2017-10-01.
+        Welcome to use CISDA (ChIP-Seq Data Analyzer), version 0.9.4,  2018-02-01.
         CISDA is a Pipeline for Single-end and Paired-end ChIP-Seq Data Analysis by Integrating Lots of Softwares.
                                                             
-        Step 11: Call peaks for BAM or BAMPE by using MACS2.  
+        Step 8: Call peaks for BAM or BAMPE by using MACS2.  
 
         Usage:
-               perl  CISDA11A.pl    [-version]    [-help]   [-genome RefGenome]    [-in inputDir]    [-out outDir]
+               perl  CISDA8A.pl    [-version]    [-help]   [-genome RefGenome]    [-in inputDir]    [-out outDir]
         For instance:
-               perl  CISDA11A.pl   -genome hg38   -in 7-finalBAM/1_Trim_BWAmem/callPeaks   -out 12A-MACS2/1_Trim_BWAmem    > CISDA11A.runLog
+               perl  CISDA8A.pl   -genome hg38   -in 6-BAMPE/1_BWAmem   -out 9A-MACS2/1_BWAmem    > CISDA8A.runLog
 
         ----------------------------------------------------------------------------------------------------------
         Optional arguments:
@@ -55,7 +56,7 @@ my $HELP = '
 ';
 
 ## Version Infromation
-my $version = "    The Eleventh Step of CISDA (ChIP-Seq Data Analyzer), version 0.9.0, 2017-10-01.";
+my $version = "    The 8th Step of CISDA (ChIP-Seq Data Analyzer), version 0.9.4,  2018-02-01.";
 
 ## Keys and Values
 if ($#ARGV   == -1)   { say  "\n$HELP\n";  exit 0;  }       ## when there are no any command argumants.
@@ -63,9 +64,9 @@ if ($#ARGV%2 ==  0)   { @ARGV = (@ARGV, "-help") ;  }       ## when the number o
 my %args = @ARGV;
 
 ## Initialize  Variables
-$genome_g = 'hg38';                                 ## This is only an initialization value or suggesting value, not default value.
-$input_g  = '7-finalBAM/1_Trim_BWAmem/callPeaks';   ## This is only an initialization value or suggesting value, not default value.
-$output_g = '12A-MACS2/1_Trim_BWAmem';              ## This is only an initialization value or suggesting value, not default value.
+$genome_g = 'hg38';               ## This is only an initialization value or suggesting value, not default value.
+$input_g  = '6-BAMPE/1_BWAmem';   ## This is only an initialization value or suggesting value, not default value.
+$output_g = '9A-MACS2/1_BWAmem';  ## This is only an initialization value or suggesting value, not default value.
 
 ## Available Arguments
 my $available = "   -version    -help   -genome   -in   -out  ";
@@ -75,7 +76,7 @@ while( my ($key, $value) = each %args ) {
 }
 if($boole == 1) {
     say  "\tThe Command Line Arguments are wrong!";
-    say  "\tPlease see help message by using 'perl  CISDA11A.pl  -help' \n";
+    say  "\tPlease see help message by using 'perl  CISDA8A.pl  -help' \n";
     exit 0;
 }
 
@@ -155,7 +156,6 @@ sub fullPathApp  {
 }
 
 &printVersion("macs2  --version");
-
 ###################################################################################################################################################################################################
 
 
@@ -261,7 +261,7 @@ for ( my $i=0; $i<=$#groupFiles_g; $i++ ) {
     $noteGroup = $n1; 
 }
 
-($#input_group_g == $numGroup-1)   or  die  "\n\n##$#input_group_g,\t$numGroup##\n\n";
+## ($#input_group_g == $numGroup-1)   or  die  "\n\n##$#input_group_g,\t$numGroup##\n\n";
 open(runFH, ">", "$output2_g/chip_input.txt")  or die;
 
 for ( my $i=0; $i<=$#input_group_g; $i++ ) { 
@@ -286,6 +286,31 @@ print("\n\ngenome_size: $genome_size_g\n\n");
 
 my $format_g = "BAMPE";   ## --format {AUTO,BAM,SAM,BED,ELAND,ELANDMULTI,ELANDEXPORT,BOWTIE,BAMPE,BEDPE}
 
+
+
+sub whichNearestString  {
+    my @list = @_;
+    my $ref_string = $list[0];
+    my @string_array = @list; 
+    my $similarity_temp = 0;
+    my $target_string;
+    #say($ref_string);
+    #say(@string_array); 
+    #say( $#string_array );  
+    for(my $i=1; $i<=$#string_array; $i++) {
+        my $similarity_1 = String::Similarity::similarity($ref_string, $string_array[$i]);
+        if($similarity_1 > $similarity_temp) {$target_string = $string_array[$i]; $similarity_temp = $similarity_1; }
+    }
+
+    return($target_string);   
+}
+
+say("\n\nInput of each sample:");
+for ( my $i=0; $i<=$#groupFiles_g; $i++ ) { 
+    $groupFiles_g[$i] =~ m/^(\d+)_($pattern_g)_(Rep[1-9])/  or  die;
+    my $input_1 = &whichNearestString($groupFiles_g[$i], @input_group_g ); 
+    print  "\t$groupFiles_g[$i],\t$input_1 ......\n";  
+}
 ###################################################################################################################################################################################################
 
 
@@ -306,17 +331,15 @@ my $outputDir2 = "$output_g/1-Peaks/2-broad";
 
 for ( my $i=0; $i<=$#groupFiles_g; $i++ ) { 
     $groupFiles_g[$i] =~ m/^(\d+)_($pattern_g)_(Rep[1-9])/  or  die;
-    my $n1 = $1;
-    $n1>=1  or  die;
-    $n1--;
-    print  "\t$groupFiles_g[$i],\t$input_group_g[$n1] ......\n";
+    my $input_1 = &whichNearestString($groupFiles_g[$i], @input_group_g ); 
+    print  "\t$groupFiles_g[$i],\t$input_1 ......\n";
     my $temp = $groupFiles_g[$i];
     $temp =~ s/\.bam$//  or  die;
     &myMakeDir("$outputDir1/$temp");
     &myMakeDir("$outputDir2/$temp");
 
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir1/$temp    --name $temp   --bw 300    --mfold 5 50   --qvalue 0.05                        >> $outputDir1/$temp.runLog    2>&1`);                    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir2/$temp    --name $temp   --bw 300    --mfold 5 50   --qvalue 0.05      --broad           >> $outputDir2/$temp.runLog    2>&1`);                    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir1/$temp    --name $temp   --bw 300    --mfold 5 50   --qvalue 0.05                        >> $outputDir1/$temp.runLog    2>&1`);                    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir2/$temp    --name $temp   --bw 300    --mfold 5 50   --qvalue 0.05      --broad           >> $outputDir2/$temp.runLog    2>&1`);                    
 }
 
 }
@@ -328,7 +351,7 @@ for ( my $i=0; $i<=$#groupFiles_g; $i++ ) {
 
 ###################################################################################################################################################################################################
 say   "\n\n\n\n\n\n##################################################################################################";
-say   "Peak calling ......";
+say   "Peak calling for more ......";
 
 {
 &myMakeDir("$output_g/1-Peaks");
@@ -366,10 +389,8 @@ my $outputDir15 = "$output_g/1-Peaks/run15";
 
 for ( my $i=0; $i<=$#groupFiles_g; $i++ ) { 
     $groupFiles_g[$i] =~ m/^(\d+)_($pattern_g)_(Rep[1-9])/  or  die;
-    my $n1 = $1;
-    $n1>=1  or  die;
-    $n1--;
-    print  "\t$groupFiles_g[$i],\t$input_group_g[$n1] ......\n";
+    my $input_1 = &whichNearestString($groupFiles_g[$i], @input_group_g ); 
+    print  "\t$groupFiles_g[$i],\t$input_1 ......\n";
     my $temp = $groupFiles_g[$i];
     $temp =~ s/\.bam$//  or  die;
     &myMakeDir("$outputDir1/$temp");
@@ -388,23 +409,23 @@ for ( my $i=0; $i<=$#groupFiles_g; $i++ ) {
     &myMakeDir("$outputDir14/$temp");
     &myMakeDir("$outputDir15/$temp");
 
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir1/$temp    --name $temp   --bw 300    --mfold 5 50   --qvalue 0.1                 >> $outputDir1/$temp.runLog    2>&1`);                    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir2/$temp    --name $temp   --bw 300    --mfold 5 50   --qvalue 0.01                >> $outputDir2/$temp.runLog    2>&1`);                    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir3/$temp    --name $temp   --bw 300    --mfold 5 50   --pvalue 0.01                 >> $outputDir3/$temp.runLog    2>&1`);                    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir4/$temp    --name $temp   --bw 300    --mfold 5 50   --pvalue 0.001                >> $outputDir4/$temp.runLog    2>&1`);    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir5/$temp    --name $temp   --bw 300    --mfold 5 50   --pvalue 1e-5                 >> $outputDir5/$temp.runLog    2>&1`);   
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir1/$temp    --name $temp   --bw 300    --mfold 5 50   --qvalue 0.1                 >> $outputDir1/$temp.runLog    2>&1`);                    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir2/$temp    --name $temp   --bw 300    --mfold 5 50   --qvalue 0.01                >> $outputDir2/$temp.runLog    2>&1`);                    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir3/$temp    --name $temp   --bw 300    --mfold 5 50   --pvalue 0.01                 >> $outputDir3/$temp.runLog    2>&1`);                    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir4/$temp    --name $temp   --bw 300    --mfold 5 50   --pvalue 0.001                >> $outputDir4/$temp.runLog    2>&1`);    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir5/$temp    --name $temp   --bw 300    --mfold 5 50   --pvalue 1e-5                 >> $outputDir5/$temp.runLog    2>&1`);   
     
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir6/$temp    --name $temp   --bw 300    --mfold 5 50   --qvalue 0.1      --broad                >> $outputDir6/$temp.runLog    2>&1`);                    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir7/$temp    --name $temp   --bw 300    --mfold 5 50   --qvalue 0.01     --broad                >> $outputDir7/$temp.runLog    2>&1`);                    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir8/$temp    --name $temp   --bw 300    --mfold 5 50   --pvalue 0.01     --broad                >> $outputDir8/$temp.runLog    2>&1`);                    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir9/$temp    --name $temp   --bw 300    --mfold 5 50   --pvalue 0.001    --broad                >> $outputDir9/$temp.runLog    2>&1`);    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir10/$temp   --name $temp   --bw 300    --mfold 5 50   --pvalue 1e-5     --broad                >> $outputDir10/$temp.runLog    2>&1`);   
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir6/$temp    --name $temp   --bw 300    --mfold 5 50   --qvalue 0.1      --broad                >> $outputDir6/$temp.runLog    2>&1`);                    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir7/$temp    --name $temp   --bw 300    --mfold 5 50   --qvalue 0.01     --broad                >> $outputDir7/$temp.runLog    2>&1`);                    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir8/$temp    --name $temp   --bw 300    --mfold 5 50   --pvalue 0.01     --broad                >> $outputDir8/$temp.runLog    2>&1`);                    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir9/$temp    --name $temp   --bw 300    --mfold 5 50   --pvalue 0.001    --broad                >> $outputDir9/$temp.runLog    2>&1`);    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir10/$temp   --name $temp   --bw 300    --mfold 5 50   --pvalue 1e-5     --broad                >> $outputDir10/$temp.runLog    2>&1`);   
     
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir11/$temp   --name $temp   --bw 300    --mfold 5 50   --qvalue 0.05     --broad      --nomodel  --nolambda           >> $outputDir11/$temp.runLog    2>&1`);                    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir12/$temp   --name $temp   --bw 300    --mfold 5 50   --pvalue 1e-5     --broad-cutoff 0.3     --broad               >> $outputDir12/$temp.runLog    2>&1`);                    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir13/$temp   --name $temp   --bw 300    --mfold 5 50   --pvalue 1e-5     --broad-cutoff 0.01    --broad               >> $outputDir13/$temp.runLog    2>&1`);                    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir14/$temp   --name $temp   --bw 300    --mfold 5 50   --pvalue 1e-5     --broad-cutoff 0.001   --broad               >> $outputDir14/$temp.runLog    2>&1`);    
-    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_group_g[$n1]   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir15/$temp   --name $temp   --bw 300    --mfold 5 50   --pvalue 1e-5     --broad-cutoff 0.0001  --broad               >> $outputDir15/$temp.runLog    2>&1`);   
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir11/$temp   --name $temp   --bw 300    --mfold 5 50   --qvalue 0.05     --broad      --nomodel  --nolambda           >> $outputDir11/$temp.runLog    2>&1`);                    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir12/$temp   --name $temp   --bw 300    --mfold 5 50   --pvalue 1e-5     --broad-cutoff 0.3     --broad               >> $outputDir12/$temp.runLog    2>&1`);                    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir13/$temp   --name $temp   --bw 300    --mfold 5 50   --pvalue 1e-5     --broad-cutoff 0.01    --broad               >> $outputDir13/$temp.runLog    2>&1`);                    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir14/$temp   --name $temp   --bw 300    --mfold 5 50   --pvalue 1e-5     --broad-cutoff 0.001   --broad               >> $outputDir14/$temp.runLog    2>&1`);    
+    system(`macs2  callpeak   --treatment $input_g/$temp.bam    --control $input_g/$input_1   --format $format_g   --gsize $genome_size_g   --keep-dup 1     --outdir $outputDir15/$temp   --name $temp   --bw 300    --mfold 5 50   --pvalue 1e-5     --broad-cutoff 0.0001  --broad               >> $outputDir15/$temp.runLog    2>&1`);   
 
  }
 
